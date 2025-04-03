3902cd0f4a2604dd592a4d6dcea662ab3c161f1e
MingLi9 <mingjanssen9@gmail.com>
2025-04-03 17:17:49 +0200

    Demo RebbitMQ service
    Spring boot service connection with RabbitMQ (can connect with Postman to check)

---
+++ DemoRabbitMQ/src/main/java/com/example/DemoRabbitMQ/Receiver.java
@@ -0,0 +1,21 @@
+package com.example.DemoRabbitMQ;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @RabbitListener(queues = "suara-queue")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
---
+++ DemoRabbitMQ/src/test/java/com/example/DemoRabbitMQ/DemoRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
