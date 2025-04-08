e27e1b42d12265d83ebbca50e35393b8efa6e722
MingLi9 <mingjanssen9@gmail.com>
2025-04-08 10:14:52 +0200

    Progress save
    Backend is working but frontend needs correct connection

---
+++ DemoService/src/main/java/com/SuaraCloud/DemoService/RabbitMQ/MessagingRabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.SuaraCloud.DemoService.RabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRabbitmqApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MessagingRabbitmqApplication.class, args);
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
 import './App.css';
 import gatewayUrl from './config';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  // sent api call to the gateway url using Axios
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.post(`${gatewayUrl}/demo/mqtt`, { data: 'test' })
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       gateway_url = {gatewayUrl}
+      data = {data}
     </div>
   );
 }
