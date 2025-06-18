7edc0cd8ba2f3e8b6a78a371bdc6e9a38fd54a29
MingLi9 <mingjanssen9@gmail.com>
2025-06-18 15:58:58 +0200

    test

--- frontend/E2E/example.spec.js
+++ frontend/E2E/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login-Logout', async ({ page }) => {
-    await page.goto('http://localhost:3000/login');
+    await page.goto('http://localhost:80/login');
     await page.getByRole('textbox', { name: 'Email address' }).click();
     await page
         .getByRole('textbox', { name: 'Email address' })
@@ -10,12 +10,12 @@ test('Login-Logout', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('#0909Ming');
     await page.getByTestId('login-submit').click();
-    await page.getByRole('heading', { name: 'Recently Played' }).click();
+    await page.getByRole('heading', { n80ame: 'Recently Played' }).click();
     await page.getByRole('button', { name: 'Logout' }).click();
 });
 
-test('test-page', async ({ page }) => {
-    await page.goto('http://localhost:3000/login');
+test('test-page', async ({ page }) => 0{
+    await page.goto('http://localhost:80/login');
     await page.getByRole('textbox', { name: 'Email address' }).click();
     await page
         .getByRole('textbox', { name: 'Email address' })
@@ -24,6 +24,6 @@ test('test-page', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).fill('#0909Ming');
     await page.getByTestId('login-submit').click();
     await page.getByRole('heading', { name: 'Recently Played' }).click();
-    await page.goto('http://localhost:3000/test');
+    await page.goto('http://localhost:80/test');
     await page.getByRole('button', { name: '128k' }).click();
 });
